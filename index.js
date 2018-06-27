9da9a03a1b28c663fa39df3cd108060d6ca367fb
naizal Edy <pritiadhikari@gmail.com>
2018-06-27 20:55:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function updateObjectWithKeyAndValue(object, key, value)
 {
-  
+  return Object.assign({}, object,{[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
-  
+ object[key]=value
+ 
+ return object;
 }
 
 function deleteFromObjectByKey(object, key)
@@ -15,5 +17,8 @@ function deleteFromObjectByKey(object, key)
 
 function destructivelyDeleteFromObjectByKey(object, key)
 {
+  object[key]=value
+ 
+ return object;
   
 }
